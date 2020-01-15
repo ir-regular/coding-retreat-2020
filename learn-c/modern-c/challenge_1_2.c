e184067ceab94f7bddd5e6ac441d98cffa884791
Jane Olszewska <3jane@3jane.co.uk>
2020-01-15 13:22:37 +0000

    Refactoring: challenge 1.2, generic partition

--- learn-c/modern-c/challenge_1_2.c
+++ learn-c/modern-c/challenge_1_2.c
@@ -1,14 +1,17 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include "mc_gen.h"
+#include "mc_sort.h"
 
 bool is_sorted(size_t list_len, int list[list_len]);
 void quick_sort(size_t len, int list[len]);
-void quick_sort2(size_t start, size_t end, int *list);
-size_t partition(size_t start, size_t end, int *list);
+void quick_sort2(size_t start, size_t end, void *list);
+void *partition(void *start, void *end, size_t size,
+        int (*compare)(void const *a, void const *b));
 void swap(void *a, void *b, void *buf, size_t size);
 
 void test_int_sort(size_t const list_len);
@@ -43,21 +46,22 @@ void test_swap(void)
 
 void test_partition(void)
 {
-        size_t pivot = 0;
+        size_t const size = sizeof(int);
+        void *pivot = 0;
 
         int list2[2] = { 4, 2 };
-        pivot = partition(0, 1, list2);
-        assert(0 == pivot);
+        pivot = partition(list2, list2 + 1, size, mc_int_compare);
+        assert(list2 == pivot);
         assert(list2[0] == 2 && list2[1] == 4);
 
         int list3[3] = { 4, 3, 1};
-        pivot = partition(0, 2, list3);
-        assert(1 == pivot);
+        pivot = partition(list3, list3 + 2, size, mc_int_compare);
+        assert(list3 + 1 == pivot); // this fails
         assert(list3[0] == 1 && list3[1] == 3 && list3[2] == 4);
 
         int list4[4] = { 1, 3, 2, 0 };
-        pivot = partition(0, 3, list4);
-        assert(2 == pivot);
+        pivot = partition(list4, list4 + 3, size, mc_int_compare);
+        assert(list4 + 2 == pivot);
         assert(list4[0] == 1 && list4[1] == 0 && list4[2] == 2 && list4[3] == 3);
 }
 
@@ -87,13 +91,18 @@ void quick_sort(size_t len, int *list)
         quick_sort2(0, len - 1, list);
 }
 
-void quick_sort2(size_t start, size_t end, int *list)
+void quick_sort2(size_t start, size_t end, void *list)
 {
 	if (end <= start) {
 		return;
 	}
 
-	size_t pivot_i = partition(start, end, list);
+        size_t const size = sizeof(int);
+
+        unsigned char *list_p = list;
+	unsigned char *pivot_p = partition(list_p + start * size, list_p + end * size, size, mc_int_compare);
+
+	ptrdiff_t pivot_i = (pivot_p - list_p) / size;
 
 	if (pivot_i > 0) {
 		quick_sort2(start, pivot_i, list);
@@ -102,31 +111,43 @@ void quick_sort2(size_t start, size_t end, int *list)
 	quick_sort2(pivot_i + 1, end, list);
 }
 
-size_t partition(size_t start, size_t end, int *list)
+void *partition(void *start, void *end, size_t size,
+        int (*compare)(void const *a, void const *b))
 {
-        size_t const size = sizeof(int);
-	int pivot = list[(start + end) / 2];
-	int less_than = start - 1; // can be -1 when just starting out
-	int more_than = end + 1;
+        unsigned char *start_p = start;
+        unsigned char *end_p = end;
+
+        // you'd think you could eliminate size by combining the lines below,
+        // but no: mid_index calculation uses integer division which rounds
+        // down to 0
+        size_t mid_index = (end_p - start_p) / (size * 2);
+        unsigned char *pivot_source = start_p + mid_index * size;
+
+        unsigned char *pivot = malloc(size);
+        memmove(pivot, pivot_source, size);
+
+	unsigned char *less_than = start_p - size; // can be -1 when just starting out
+	unsigned char *more_than = end_p + size;
 	void *swap_buf = malloc(size);
 
 	while (true) {
 		do {
-			less_than++;
-		} while (list[less_than] < pivot);
+			less_than += size;
+		} while (compare(less_than, pivot) == -1);
 
 		do {
-			more_than--;
-		} while (list[more_than] > pivot);
+			more_than -= size;
+		} while (compare(more_than, pivot) == 1);
 
 		if (more_than <= less_than) {
 			break;
 		}
 
-		swap((void *)list + less_than * size, (void *)list + more_than * size, swap_buf, size);
+                swap(less_than, more_than, swap_buf, size);
 	}
 
 	free(swap_buf);
+	free(pivot);
 
 	return more_than;
 }
