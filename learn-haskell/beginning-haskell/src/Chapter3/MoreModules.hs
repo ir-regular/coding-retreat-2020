9d3ebb3515341cd5dc1d4867ab87dacab5a10a52
Jane Olszewska <3jane@3jane.co.uk>
2020-02-03 21:51:19 +0000

    "Beginning Haskell", Chapter 3, part 2

--- learn-haskell/beginning-haskell/src/Chapter3/MoreModules.hs
+++ learn-haskell/beginning-haskell/src/Chapter3/MoreModules.hs
@@ -3,7 +3,8 @@
 module Chapter3.MoreModules where
 
 import Data.List
-import Chapter2.SimpleFunctions (Client(..), clientName2)
+import Data.Maybe
+import Chapter2.SimpleFunctions (Client(..), Person(..), Gender(..), clientName2)
 
 -- example from the book
 --
@@ -96,3 +97,95 @@ minimumClientFold xs = foldl shorterName Nothing xs
 
 conjunctionFold :: [Bool] -> Bool
 conjunctionFold = foldl (&&) True
+
+partition' :: (a -> Bool) -> [a] -> ([a], [a])
+partition' p = let update x (yes,no) = if p x then (x:yes,no) else (yes,x:no)
+               in foldr update ([], [])
+
+find' :: (a -> Bool) -> [a] -> Maybe a
+find' p []     = Nothing
+find' p (x:xs) = if p x then Just x else find' p xs
+
+-- OR
+
+find'' :: (a -> Bool) -> [a] -> Maybe a
+find'' p = listToMaybe . dropWhile (\x -> not (p x))
+
+dropWhile' :: (a -> Bool) -> [a] -> [a]
+dropWhile' p []         = []
+dropWhile' p all@(x:xs) = if p x then dropWhile' p xs else all
+
+takeWhile' :: (a -> Bool) -> [a] -> [a]
+takeWhile' p []     = []
+takeWhile' p (x:xs) = if p x then x : takeWhile' p xs else []
+
+span' :: (a -> Bool) -> [a] -> ([a], [a])
+span' p [] = ([], [])
+span' p all@(x:xs) = if p x then (x : taken, rest) else ([], all)
+                     where (taken, rest) = span' p xs
+
+break' :: (a -> Bool) -> [a] -> ([a], [a])
+break' p = span' (not . p)
+
+any' :: (a -> Bool) -> [a] -> Bool
+any' p = isJust . find' p
+
+all' :: (a -> Bool) -> [a] -> Bool
+all' p = isNothing . find' (not . p)
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' x xs = isJust (find' (== x) xs)
+
+-- this doesn't preserve the order
+nub' :: (Eq a) => [a] -> [a]
+nub' []     = []
+nub' (x:xs) = if elem' x xs then nub' xs else x : nub' xs
+
+-- this does, but also is slower
+nub'' :: (Eq a) => [a] -> [a]
+nub'' []     = []
+nub'' (x:xs) = if elem' x xs then x : nub'' (filter (/= x) xs) else x : nub'' xs
+
+nubBy' :: (a -> a -> Bool) -> [a] -> [a]
+nubBy' c []     = []
+nubBy' c (x:xs) = if isJust $ find' (c x) xs then nubBy' c xs else x : nubBy' c xs
+
+--  - `insert` - adds one element to set if not exists
+--  - `insertBy` - adds one element to set if not exists, using custom comparator
+
+union' :: (Eq a) => [a] -> [a] -> [a]
+union' l []  = l
+union' [] l  = l
+union' l1 l2 = l1 ++ (filter (\x -> not $ elem' x l1) l2)
+
+intersect' :: (Eq a) => [a] -> [a] -> [a]
+intersect' l1 l2 = filter (\x -> elem' x l1) l2
+
+(\\\) :: (Eq a) => [a] -> [a] -> [a]
+(\\\) l1 [] = l1
+(\\\) l1 l2 = filter (\x -> not $ elem' x l2) l1
+
+insert' :: (Ord a) => a -> [a] -> [a]
+insert' e all@(x:xs)
+  | e > x     = x : (insert' e xs)
+  | otherwise = e : all
+
+compare' :: (Ord a) => a -> a -> Ordering
+compare' x y = if x < y then LT
+               else if x > y then GT
+               else EQ
+
+groupBy' :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy' c [] = []
+groupBy' c xs = let x             = head xs
+                    (xlike, rest) = span' (c x) xs
+                in
+                    (xlike) : groupBy' c rest
+
+zip' :: [a] -> [b] -> [(a,b)]
+zip' [] []         = []
+zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+delete' :: (Eq a) => a -> [a] -> [a]
+delete' x xs = preX ++ postX
+              where (preX, _:postX) = break' (== x) xs
